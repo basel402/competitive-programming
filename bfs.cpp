2623861ed128aec77186b4ae6bcdb1e018659cee
Basel_Holaiel <143265738+basel402@users.noreply.github.com>
2025-03-25 12:51:54 +0200

    Update bfs.cpp

--- bfs.cpp
+++ bfs.cpp
@@ -1,6 +1,8 @@
 // bfs is to determine the shortest path from the source node to the other nodes
 // if i made bfs and i found some nodes their values are 1e18 in the dist arrat then i cant reach this node from the source node
-// u can print the shortest path form a node to a other by doing bfs then make a parent array and while pudhing in the queue make the parent of the u  = cur
+// u can print the shortest path form a node to a other by doing bfs then make a parent array all at first = -1 and then assign the parents to all the childs and then traverse from the last node until the parent of the cur node is -1 which is the root
+// if the cost of the edges is 1 then do the normal bfs but if the cost of a certain edge is 0 then push front not push back so we will use deque
+// multiple source problems you will bfs from all the sources at the same time and minimize the dist as usual
 ll n,m;
 vector<ll>adj[sz];
 vector<ll>dist(sz,1e9);
