63e2f2dbea03ad8da6d348406dbc4a4341f5f37c
Basel_Holaiel <143265738+basel402@users.noreply.github.com>
2025-04-23 20:44:39 +0200

    Update dfs.cpp

--- dfs.cpp
+++ dfs.cpp
@@ -6,9 +6,69 @@
 // in grid problems use nx and ny arrays intead of using adj list to know connected cells, they are known you can add 1 or -1 or add 0 .. to know your connected cells instead of using great memory and time in adj list
 // dfs and bfs are traversal algorithms on a graph, dfs pass by all nodes in a graph dont care about order it cares about how adj list is built
 // to check if the graph is a tree you will check if it have n-1 edges and acyclic or it have n-1 edges and connected
+
+//n-1 edges and acyclic check
+bool flag = true;
+void dfs(int v, int parent){
+
+    vis[v] = true;
+    for(auto it : adj[v]){
+        if(!vis[it] && flag)dfs(it,v);
+        else if(it!=parent && flag){
+            flag = false;
+            return;
+        }
+    }
+}
+
+void solve() {
+  ll n,m;cin >> n >> m;
+    for (int i = 0; i < m; ++i) {
+        ll a,b;cin >> a >> b;
+        adj[a].push_back(b);
+        adj[b].push_back(a);
+    }
+    if(m!=n-1){
+        no;
+        return;
+    }
+    for (int i = 1; i <=n; ++i) {
+        if(!vis[i])dfs(i,-1);
+    }
+    if(!flag)no;
+    else yes;
+
+}
+
+// n-1 edges and connected check
+ll vs = 0;
+void dfs(int v, int parent){
+
+    vs++;
+    for(auto it : adj[v]){
+        if(it!=parent)dfs(it,v);
+    }
+}
+
+void solve() {
+  ll n,m;cin >> n >> m;
+    for (int i = 0; i < m; ++i) {
+        ll a,b;cin >> a >> b;
+        adj[a].push_back(b);
+        adj[b].push_back(a);
+    }
+    if(m!=n-1){
+        no;
+        return;
+    }
+    dfs(1,-1);
+    if(vs < n)no;
+    else yes;
+
+}
+
 // to check its acyclic you will loop on all nodes and check if vis and not the parent then there is a cycle and return else dfs 
 // dfs time complexity is o(vert + edges)
-
 // dfs code on a graph(tree,...)
 const int sz = 2e5+5;
 vector<ll>adj[sz];
