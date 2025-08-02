d9b162d5ae6362ab4fb4a35724a6032929ccc50c
Basel_Holaiel <143265738+basel402@users.noreply.github.com>
2025-08-02 18:15:38 +0300

    Update sparse table.cpp

--- sparse table.cpp
+++ sparse table.cpp
@@ -2,7 +2,7 @@ const int N = 1e5+5;
 const int lg = 20;
 ll sp[N][lg];
 ll a[N],n;
-
+// for any operation $ where x $ x = x, you can make a sparse table to answer quereies for it
 void pre(){
     for (int i = 0; i < lg; ++i) {
         for (int j = 0; j + (1 << i) - 1 < n; ++j) {
