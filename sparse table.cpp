8f8cab2766d0b7fcd789906f41063d26bb13ec4a
Basel_Holaiel <143265738+basel402@users.noreply.github.com>
2025-08-02 23:52:31 +0300

    Update sparse table.cpp

--- sparse table.cpp
+++ sparse table.cpp
@@ -1,3 +1,4 @@
+const int MOD = 1e9 + 7;
 const int N = 1e5+5;
 const int lg = 20;
 ll sp[N][lg];
@@ -11,23 +12,12 @@ void pre(){
                 continue;
             }
             if(j + (1 << (i-1)) < N)
-                sp[j][i] = min(sp[j][i-1],sp[j + (1 << (i-1))][i-1]);
+                sp[j][i] = gcd(sp[j][i-1],sp[j + (1 << (i-1))][i-1]);
         }
     }
 }
 
-void solve() {
-
-    cin >> n;
-    for (int i = 0; i < n; ++i) {
-        cin >> a[i];
-    }
-    pre();
-    ll q;cin >> q;
-    while(q--){
-        ll l,r;cin >> l >> r;
-        ll k = 31 - __builtin_clz(r-l+1);
-        cout << min (sp[l][k],sp[r - (1 << k) + 1][k]) << endl;
-    }
-
+ll query(ll l, ll r){
+    ll k = 31 - __builtin_clz(r-l+1);
+    return gcd (sp[l][k],sp[r - (1 << k) + 1][k]);
 }
